db305e06a37e4d5afcdbef056181cafe7ca93b0f
Oguzhan Ince <oguzhan824@gmail.com>
2020-05-19 06:34:31 +0300

    added mockData file

---
+++ mockData.ts
@@ -0,0 +1,93 @@
+interface MockData {
+  id: Number;
+  first_name: String;
+  last_name: String;
+  username: String;
+  email: String;
+  gender: String;
+}
+
+export let mockData: Array<MockData> = [
+  {
+    "id": 1,
+    "first_name": "Charo",
+    "last_name": "Genney",
+    "username": "cgenney0",
+    "email": "cgenney0@columbia.edu",
+    "gender": "Female",
+  },
+  {
+    "id": 2,
+    "first_name": "Sammy",
+    "last_name": "Wabersinke",
+    "username": "swabersinke1",
+    "email": "swabersinke1@oracle.com",
+    "gender": "Male",
+  },
+  {
+    "id": 3,
+    "first_name": "Iago",
+    "last_name": "Szymanowicz",
+    "username": "iszymanowicz2",
+    "email": "iszymanowicz2@drupal.org",
+    "gender": "Male",
+  },
+  {
+    "id": 4,
+    "first_name": "Rafi",
+    "last_name": "O'Hannen",
+    "username": "rohannen3",
+    "email": "rohannen3@ucoz.com",
+    "gender": "Male",
+  },
+  {
+    "id": 5,
+    "first_name": "Noelyn",
+    "last_name": "Gouley",
+    "username": "ngouley4",
+    "email": "ngouley4@dmoz.org",
+    "gender": "Female",
+  },
+  {
+    "id": 6,
+    "first_name": "Crawford",
+    "last_name": "Weigh",
+    "username": "cweigh5",
+    "email": "cweigh5@list-manage.com",
+    "gender": "Male",
+  },
+  {
+    "id": 7,
+    "first_name": "Carroll",
+    "last_name": "Bredee",
+    "username": "cbredee6",
+    "email": "cbredee6@reddit.com",
+    "gender": "Male",
+  },
+  {
+    "id": 8,
+    "first_name": "Jordon",
+    "last_name": "Fishburn",
+    "username": "jfishburn7",
+    "email": "jfishburn7@amazonaws.com",
+    "gender": "Male",
+  },
+  {
+    "id": 9,
+    "first_name": "Lutero",
+    "last_name": "Wyvill",
+    "username": "lwyvill8",
+    "email": "lwyvill8@plala.or.jp",
+    "gender": "Male",
+  },
+  {
+    "id": 10,
+    "first_name": "Yule",
+    "last_name": "Comrie",
+    "username": "ycomrie9",
+    "email": "ycomrie9@storify.com",
+    "gender": "Male",
+  },
+];
+
+// TODO : GET, POST, PUT, DELETE
