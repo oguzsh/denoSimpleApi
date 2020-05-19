e769757439be86d7da073964538761ac4c10988b
Oguzhan Ince <oguzhan824@gmail.com>
2020-05-19 06:33:42 +0300

    added mockData in index file

--- index.ts
+++ index.ts
@@ -3,13 +3,21 @@ import {
   Router,
 } from "https://deno.land/x/denotrain@v0.4.4/mod.ts";
 
+import { mockData } from "./mockData.ts";
+
 // default port : 3000, host: 0.0.0.0
 const app = new Application({ port: 8000 });
 
 const router = new Router();
 
-app.get("/", (ctx) => {
-  return { user: "oguzsh" };
+app.get("/", () => {
+  return "Welcome to mock data api, go /api/data";
+});
+
+app.use("/api", router);
+
+router.get("/data", () => {
+  return { data: mockData };
 });
 
 await app.run();
