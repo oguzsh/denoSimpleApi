8e7c149fe24dc5dd61440dbd8365d8b4482bb54c
Oguzhan Ince <oguzhan824@gmail.com>
2020-05-19 20:48:46 +0300

    added models

---
+++ models/mockModel.ts
@@ -0,0 +1,8 @@
+export default class MockModel {
+  id: Number = 0;
+  first_name: String = "";
+  last_name: String = "";
+  username: String = "";
+  email: String = "";
+  gender: String = "";
+}
